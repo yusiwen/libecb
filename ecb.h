f9e2f0654cfc0ef0eed4112520c6e13b5adbded3
root <root>
2011-05-26 17:38:25 +0000

    *** empty log message ***

--- ecb.h
+++ ecb.h
@@ -2,6 +2,7 @@
  * libecb
  *
  * Copyright (©) 2009-2011 Marc Alexander Lehmann
+ * Copyright (©) 2011 Emanuele Giaquinta
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modifica-
