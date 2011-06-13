9d746ee0840dbb8bba23d86a7aef929234d633f2
root <root>
2011-06-13 08:19:29 +0000

    painful

--- ecb.h
+++ ecb.h
@@ -30,7 +30,23 @@
 #ifndef ECB_H
 #define ECB_H
 
-#include <inttypes.h>
+#ifdef _WIN32
+  typedef   signed char   int8_t;
+  typedef unsigned char  uint8_t;
+  typedef   signed short  int16_t;
+  typedef unsigned short uint16_t;
+  typedef   signed int    int32_t;
+  typedef unsigned int   uint32_t;
+  #if __GNUC__
+    typedef   signed long long int64_t;
+    typedef unsigned long long uint64_t;
+  #else
+    typedef   signed __int64   int64_t;
+    typedef unsigned __int64   uint64_t;
+  #endif
+#else
+  #include <inttypes.h>
+#endif
 
 /* many compilers define _GNUC_ to some versions but then only implement
  * what their idiot authors think are the "more important" extensions,
