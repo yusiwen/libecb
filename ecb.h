48b6cdadcf1421854402b2fd54852e32ee31c94a
root <root>
2012-01-25 19:57:49 +0000

    *** empty log message ***

--- ecb.h
+++ ecb.h
@@ -388,8 +388,8 @@ ecb_inline uint64_t ecb_rotr64 (uint64_t x, unsigned int count) { return (x << (
   #define ecb_unreachable() __builtin_unreachable ()
 #else
   /* this seems to work fine, but gcc always emits a warning for it :/ */
-  ecb_function_ void ecb_unreachable (void) ecb_noreturn;
-  ecb_function_ void ecb_unreachable (void) { }
+  ecb_inline void ecb_unreachable (void) ecb_noreturn;
+  ecb_inline void ecb_unreachable (void) { }
 #endif
 
 /* try to tell the compiler that some condition is definitely true */
