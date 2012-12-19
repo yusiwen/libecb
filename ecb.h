5f4acb87b32328a694f9d3df521489a01c580dbe
root <root>
2012-12-19 23:51:24 +0000

    §

--- ecb.h
+++ ecb.h
@@ -68,9 +68,9 @@
 /* work around x32 idiocy by defining proper macros */
 #if __x86_64 || _M_AMD64
   #if __ILP32
-    #define ECB_AMD64_X32
+    #define ECB_AMD64_X32 1
   #else
-    #define ECB_AMD64
+    #define ECB_AMD64 1
   #endif
 #endif
 
