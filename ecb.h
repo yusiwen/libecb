13fd035d07bb82db48c76a3cf22e019f1af722d9
root <root>
2014-03-21 16:40:45 +0000

    input by Riku Voipio

--- ecb.h
+++ ecb.h
@@ -605,7 +605,8 @@ ecb_inline ecb_bool ecb_little_endian (void) { return ecb_byteorder_helper () ==
     || defined __m68k__ \
     || defined __m88k__ \
     || defined __sh__ \
-    || defined _M_IX86 || defined _M_AMD64 || defined _M_IA64
+    || defined _M_IX86 || defined _M_AMD64 || defined _M_IA64 \
+    || (defined __arm__ && (defined __ARM_EABI__ || defined __EABI__ || defined __VFP_FP__ || defined _WIN32_WCE || defined __ANDROID__))
   #define ECB_STDFP 1
   #include <string.h> /* for memcpy */
 #else
